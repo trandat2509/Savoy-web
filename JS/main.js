a99e14a311966b00ad00e236da70ffef4b590b8d
TRAN_HAU_NGUYEN_DAT <trandat25092004@gmail.com>
2023-11-28 22:32:24 +0700

    savoy web 28/11/2023

--- JS/main.js
+++ JS/main.js
@@ -75,4 +75,10 @@ $(document).ready(function () {
     e.preventDefault(); // Ngăn chặn hành động mặc định của liên kết
     $(".carts").toggle(); // Hiển thị hoặc ẩn phần carts
   });
+
+  // Xử lý sự kiện khi nhấp vào liên kết "Close" hoặc "Continue shopping" bên trong carts
+  $(".carts .close, .carts a[href='/']").click(function (e) {
+    e.preventDefault(); // Ngăn chặn hành động mặc định của liên kết
+    $(".carts").hide(); // Ẩn phần carts
+  });
 });
