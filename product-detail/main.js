f7f828af6fdd41af428f5e2d3b99c72e997c6a6d
TRAN_HAU_NGUYEN_DAT <trandat25092004@gmail.com>
2024-01-15 22:38:59 +0700

    update savoy web

--- product-detail/main.js
+++ product-detail/main.js
@@ -48,3 +48,20 @@ function toggleContent(showId, hideId) {
     hideElement.style.display = "none";
   }
 }
+
+$(document).ready(function () {
+  // Ẩn ban đầu phần carts
+  $(".carts").hide();
+
+  // Xử lý sự kiện khi nhấp vào liên kết "Cart"
+  $(".cart-action").click(function (e) {
+    e.preventDefault(); // Ngăn chặn hành động mặc định của liên kết
+    $(".carts").toggle(); // Hiển thị hoặc ẩn phần carts
+  });
+
+  // Xử lý sự kiện khi nhấp vào liên kết "Close" hoặc "Continue shopping" bên trong carts
+  $(".carts .close, .carts a[href='/']").click(function (e) {
+    e.preventDefault(); // Ngăn chặn hành động mặc định của liên kết
+    $(".carts").hide(); // Ẩn phần carts
+  });
+});
